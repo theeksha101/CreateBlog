22941f2dca9440587d075cd38baf1fb45dfb2091
theeksha101 <deekshaw25@gmail.com>
2023-10-27 17:34:41 +0530

    changed html for the site

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views  # importing all views from blog
+
+# assigning a view called post_list to the root url
+# The last part, name='post_list', is the name of the URL that will be used to identify the view
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+]
