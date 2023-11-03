5e207fb3ccfbf20fd643cbcc87c8e81b76a5cbb3
theeksha101 <deekshaw25@gmail.com>
2023-11-03 17:05:51 +0530

    Modified templates to display post from database

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,11 @@
+# views connect models and templates
+# In Django, views are responsible for processing requests and returning an HTTP response. 
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts':posts})
+# {}: This is a dictionary that can be used to pass data to the template.
