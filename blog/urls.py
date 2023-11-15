4aa41bccf288643dc00c326b58bf4f355763b8bc
theeksha101 <deekshaw25@gmail.com>
2023-11-15 17:50:23 +0530

    added view called post_details and static files base, post_details.html and updated url.py

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from . import views  # importing all views from blog
 # The last part, name='post_list', is the name of the URL that will be used to identify the view
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
